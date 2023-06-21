9c1faf5c85d876d67fab96211d784eccae7f36bc
Panda <jbcod4@gmail.com>
2023-06-20 23:44:43 -0500

    the call is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super() 
+    this.state = {
+      quotes: {}
+    }
+  }
+  componentDidMount() {
+    fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+    .then(response => {
+      if (!response.ok) { return false }
+      return response.json()
+    }).then(quotes => {
+      // console.log(quotes[0])
+      this.setState({
+        quotes : quotes[0]
+      })
+    }).catch(e => console.log('error' + e))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+  
+          <p>
+            " {this.state.quotes.quote} " <br></br>
+            - {this.state.quotes.author}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Refresh
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
